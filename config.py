07ff3ee0eafe85a9152e53b9976cc81806d9b464
Himani29-Raj <78725031+Himani29-Raj@users.noreply.github.com>
2021-06-29 19:05:43 +0530

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-PROJECT_NAME = "Sentiment Analysis In Hindi Language"
-DEVELOPER = 'Himani Raj and Deepak Vishwakarma'
-MENU_OPTIONS = ('About Project','Instruction','Sentiment Analysis','View Records')
